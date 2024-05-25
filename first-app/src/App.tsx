74cceb7657b0678f237125376153a0f0fd10d054
sid <sidshekhar2024@gmail.com>
2024-05-25 06:59:51 +0530

    create signup using email auth

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,27 +1,48 @@
-import { getDatabase ,ref,set} from "firebase/database" //one is ref of database and one to alter the db my connceting it through db
+// import { getDatabase ,ref,set} from "firebase/database" //one is ref of database and one to alter the db my connceting it through db
 import {app} from "./firebase"
 
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
+import { useState } from "react";
 
-const db = getDatabase(app) //get db from app config and connect to real time db usign config url
+
+// const db = getDatabase(app) //get db from app config and connect to real time db usign config url
+
+const auth = getAuth(app);
+
+
+// const insert = async()=>{
+//   set(ref(db, 'user/test-object3'),{
+//   id:2,
+//   name:"sid"
+// })
+
+//set used to alter the tabel adn ref take two argument of ref of db and second on has tabel ref  and naem of the obj  that you wanna save
 
 
 export default function App(){
   
 
-  const insert = async()=>{
-      set(ref(db, 'user/test-object2'),{
-      id:2,
-      name:"sid"
-    })
+ const [email,setEmail]= useState("")
+ const [pass,setPass] = useState("")
 
-    //set used to alter the tabel adn ref take two argument of ref of db and second on has tabel ref  and naem of the obj  that you wanna save
-    
-  }
+ 
+const emailLogin = async ()=>{
+  const res = await createUserWithEmailAndPassword(auth,email,pass);
+  console.log(res);
+  setEmail("")
+  setPass("")
+  
+  
+}
   
   return(
-    <>
-      <button onClick={()=>insert()}>PUT</button>
+    <div>
+
+    <label>email</label><input type="text" placeholder="email" required value={email} onChange={e=>setEmail(e.target.value)}/>
+    <label>password</label><input type="text" placeholder="password" value={pass}  required onChange={e=>setPass(e.target.value)}/>
+
+      <button onClick={()=>emailLogin()}>PUT</button>
       
-    </>
+    </div>
   )
 }
